ffce675e3852f024222e509a81321809216ed3a3
Delta <qureshimohsin958@gmail.com>
2026-05-07 13:18:45 +0530

    update

--- backend/models/User.js
+++ backend/models/User.js
@@ -29,16 +29,15 @@ const userSchema=mongoose.Schema({
     timestamps:true
 });
 
-userSchema.pre("save",async function (next) {
-      try {
-    if(!this.isModified("password")) return next();
+userSchema.pre("save",async function () {
+    
+    if(!this.isModified("password")) {return} ;
 
     const salt=await bcrypt.genSalt(10);
     this.password=await bcrypt.hash(this.password,salt);
-    next();
-      } catch (error) {
-    next(error);
-  }
+   
+    
+  
 
 })
 module.exports=mongoose.model("User",userSchema)
